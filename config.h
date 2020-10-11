7006961474b6754553b67e44a144caeda622d1d0
Filipenco Pavel <filipencop@mail.ru>
2020-10-11 14:12:31 +0300

    Changed font to Terminus; previously: Liberation Mono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true";
+static char *font = "Terminus:pixelsize=14:antialias=false";
 static int borderpx = 0;
 
 /*
@@ -25,7 +25,7 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
+static float cwscale = 0.8;
 static float chscale = 1.0;
 
 /*
